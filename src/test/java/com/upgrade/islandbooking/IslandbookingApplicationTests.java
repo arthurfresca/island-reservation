1a452ea4ba65d3a7f98264a7090b60875cd8e9cf
Arthur Mendonça Fresca <arthur.fresca@justeattakeaway.com>
2022-08-22 11:14:48 -0400

    Initial spring boot setup

---
+++ src/test/java/com/upgrade/islandbooking/IslandbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrade.islandbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslandbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
